db9ea0b1169d606b61b4e3ebc508109db304da4d
Svetlana Chenchikova <fenina.svetlana@gmail.com>
2021-08-18 17:38:00 +0300

    feat: add 3-module-1-task

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,3 +1,12 @@
+let vasya = { name: 'Вася', age: 25 };
+let petya = { name: 'Петя', age: 30 };
+let masha = { name: 'Маша', age: 28 };
+
+let users = [ vasya, petya, masha ];
+
 function namify(users) {
-  // ваш код...
+let names = users.map(item  => item.name);
+  {return names}
 }
+
+namify(users);
