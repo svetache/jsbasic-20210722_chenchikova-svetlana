232d1bbc9993e2adfca5f603e13162bb3a34d9ac
Svetlana Chenchikova <fenina.svetlana@gmail.com>
2021-08-20 15:38:35 +0300

    feat: add  3-module-3-task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,11 @@
-function camelize(str) {
-  // ваш код...
-}
+  function camelize(str) {
+    return str
+      .split('-') 
+      .map(
+        (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+      )
+      .join('');
+  }
+camelize("background-color");
+camelize("list-style-image");
+camelize("-webkit-transition");
