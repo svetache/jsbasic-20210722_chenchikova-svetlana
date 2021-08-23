7194d17a4864dcce6cbb2932d7b2fad6e978be1e
Svetlana Chenchikova <fenina.svetlana@gmail.com>
2021-08-23 15:16:41 +0300

    feat: add 3-module-tasks

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,10 @@
+let arr = [5, 3, 8, 1];
+
 function filterRange(arr, a, b) {
-  // ваш код...
+    return arr.filter(item => item <= b && item >= a);
 }
+
+let filtered = filterRange(arr, 1, 4);
+
+console.log( filtered ); 
+console.log( arr ); 
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,16 @@
+let inputData = '1 и -5.8 или 10 хотя 34 + -5.3 и 73';
+
 function getMinMax(str) {
-  // ваш код...
+  let array = str.split(/[ ,]+/);
+ 
+  let minV = str[0];
+  let maxV = str[0];
+  
+  for (let i = 0; i < array.length; i++) {
+    let a = +array[i];
+    if (a < minV) minV = a;
+    if (a > maxV) maxV = a;
+  }
+  return {min: minV, max: maxV};
 }
+console.log(getMinMax(inputData));
