9f55fb1062f076b8998cd7b3b45ab2ad5a5b4aa2
Svetlana Chenchikova <fenina.svetlana@gmail.com>
2021-08-25 14:53:20 +0300

    feat:3-module-4-task update

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,35 +1,13 @@
-let user1 = {
-  "balance": "$1,825.65",
-  "picture": "https://placehold.it/32x32",
-  "age": 21,
-  "name": "Golden Branch",
-  "gender": "male",
-  "greeting": "Hello, Golden Branch! You have 7 unread messages.",
-  "favouriteFruit": "banana"
-};
+function showSalary(data, age) {
+  let result = [];
 
-let user2 = {
-  "balance": "$1,000.07",
-  "picture": "https://placehold.it/32x32",
-  "age": 30,
-  "name": "Duncan Randall",
-  "gender": "male",
-  "greeting": "Hello, Duncan Randall! You have 2 unread messages.",
-  "favouriteFruit": "strawberry"
-};
-let users = [user1, user2];
+  data.forEach(function(user) {
+    if (user.age <= age){
+      result.push(`${user.name}, ${user.balance}`);
+    }
+  });
 
-function showSalary(users, age){
-  if (user1.age && user2.age <= age)
-  {
-    return users.map(user => `${user.name}, ${user.balance}`).join(',\n')
-  }
+  return result.join('\n');
 }
-age = 30;
-console.log(showSalary(users,30));
-age = 40;
-console.log(showSalary(users,age));
-
-
 
 
